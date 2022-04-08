4214c71469504fb175447cbb3e6166df874ab7c2
Felipe Silva <antoniofelipesilva@live.com>
2022-04-08 14:56:55 -0300

    Add eventos

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,27 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    $(document).ready(function () {})
 
+$('#carrosel img:eq(0)').addClass('banner-ativo').show()
+
+setInterval(slide, 1000)
+
+function slide() {
+  if ($('.banner-ativo').next().length) {
+    $('.banner-ativo')
+      .removeClass('banner-ativo')
+      .hide()
+      .next()
+      .addClass('banner-ativo')
+      .show()
+  } else {
+    $('.banner-ativo').removeClass().hide()
+    $('#carrosel img:eq(0)').addClass('banner-ativo').show()
+  }
+}
+$('#barras').click(function () {
+  $('#menu').toggleClass('menu-ativo')
+})
+  }
 }
